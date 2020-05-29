b9157b2f4ba7ad74b00988e4564af8003921dca6
Rene Avila <reneavila1993@gmail.com>
2020-05-29 19:18:46 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+require 'pry'
+class Song 
+  
+  attr_accessor :name, :artist
+  
+  @@all = []
+  
+  def initialize name 
+    @name = name
+    @artist = artist
+    
+    @@all << self
+  end 
+  
+  def artist_name
+    self.artist.name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
